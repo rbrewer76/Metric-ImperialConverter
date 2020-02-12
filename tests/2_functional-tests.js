4c0d0b35892b95ba73167c18d531dba5c22a9b87
Glitch (boiling-child-1) <none>
2020-02-12 19:26:02 +0000

    🍱🍋 Updated with Glitch

---
+++ tests/2_functional-tests.js
@@ -0,0 +1,85 @@
+/*
+*
+*
+*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+*       -----[Keep the tests in the same order!]-----
+*       (if additional are added, keep them at the very end!)
+*/
+
+const chaiHttp = require("chai-http")
+const chai = require("chai")
+const assert = chai.assert
+const server = require("../server")
+
+chai.use(chaiHttp)
+
+suite("Functional Tests", () => {
+
+  suite("Routing Tests", () => {
+    
+    suite("GET /api/convert => conversion object", () => {
+      
+      test("Convert 10l (valid input)", done => {
+        chai.request(server)
+          .get("/api/convert")
+          .query({input: "10l"})
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body.initNum, 10)
+            assert.equal(res.body.initUnit, "l")
+            assert.approximately(res.body.returnNum, 2.64172, 0.1)
+            assert.equal(res.body.returnUnit, "gal")
+            done()
+          })
+      })
+      
+      test("Convert 32g (invalid input unit)", done => {
+        chai.request(server)
+          .get("/api/convert")
+          .query({input: "32g"})
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.text,"invalid unit")        
+            done()
+          }) 
+      })
+      
+      test("Convert 3/7.2/4kg (invalid number)", done => {
+        chai.request(server)
+          .get("/api/convert")
+          .query({input: "3/7.2/4kg"})
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.text,"invalid number")        
+            done()
+          }) 
+      })
+      
+      test("Convert 3/7.2/4kilomegagram (invalid number and unit)", done => {
+        chai.request(server)
+          .get("/api/convert")
+          .query({input: "3/7.2/4kilomegagram"})
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.text,"invalid number and unit")        
+            done()
+          }) 
+      })
+      
+      test("Convert kg (no number)", done => {
+        chai.request(server)
+          .get("/api/convert")
+          .query({input: "kg"})
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body.initNum, 1)
+            assert.equal(res.body.initUnit, "kg")
+            assert.approximately(res.body.returnNum, 2.2046, 0.1)
+            assert.equal(res.body.returnUnit, "lbs")        
+            done()
+          })    
+      })
+      
+    })
+  })
+})
